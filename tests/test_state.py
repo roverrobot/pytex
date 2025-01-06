e1eeb551f7283087ffb3aad7279f634359f2d626
Junling Ma <junlingm@uvic.ca>
2025-01-06 09:41:00 -0800

    port test_state.py to pytest

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,90 +1,95 @@
-import unittest
+import pytest
 from pytex import state
 from pytex.parser import Parser
 
 
-class TestGroupStack(unittest.TestCase):
-    def setUp(self):
-        self.group_stack = state.GroupStack()
-        self.dict = state.Domain(name="dict", values={}, group_stack=self.group_stack)
-        self.array = state.Domain(name="array", values=[0], group_stack=self.group_stack)
+@pytest.fixture
+def groupstack():
+    stack = state.GroupStack()
+    d = state.Domain(name="dict", values={}, group_stack=stack)
+    a = state.Domain(name="array", values=[0,0,0], group_stack=stack)
+    return stack, d, a
 
-    def test_set_value(self):
-        self.dict["key1"] = "value1"
-        self.assertEqual(self.dict["key1"], "value1")
-        self.array[0] = 0
-        self.assertEqual(self.array[0], 0)
-        self.dict["key1"] = "value2"
-        self.assertEqual(self.dict["key1"], "value2")
-        self.array[0] = 1
-        self.assertEqual(self.array[0], 1)
+def test_set_value(groupstack):
+    stack, d, a = groupstack
+    d["key1"] = "value1"
+    assert d["key1"] == "value1"
+    a[1] = 1
+    assert a[1] == 1
+    d["key1"] = "value2"
+    assert d["key1"] == "value2"
+    a[1] = 0
+    assert a[1] == 0
+ 
+def test_set_in_group(groupstack):
+    stack, d, a = groupstack
+    d["key1"] = "value1"
+    a[1] = 1
+    stack.begin(group_type=state.GROUP_TYPE.SIMPLE, position=0)
+    d["key1"] = "value2"
+    assert d["key1"] == "value2"
+    a[1] = 0
+    assert a[1] == 0
+    stack.begin(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
+    d["key1"] = "value3"
+    assert d["key1"] == "value3"
+    a[1] = -1
+    assert a[1] == -1
+    stack.end(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
+    assert d["key1"] == "value2"
+    assert a[1] == 0
+    stack.end(group_type=state.GROUP_TYPE.SIMPLE, position=1)
+    assert d["key1"] == "value1"
+    assert a[1] == 1
 
-    def test_set_in_group(self):
-        self.dict["key1"] = "value1"
-        self.array[0] = 0
-        self.group_stack.begin(group_type=state.GROUP_TYPE.SIMPLE, position=0)
-        self.dict["key1"] = "value2"
-        self.assertEqual(self.dict["key1"], "value2")
-        self.array[0] = 1
-        self.assertEqual(self.array[0], 1)
-        self.group_stack.begin(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
-        self.dict["key1"] = "value3"
-        self.assertEqual(self.dict["key1"], "value3")
-        self.array[0] = 2
-        self.assertEqual(self.array[0], 2)
-        self.group_stack.end(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
-        self.assertEqual(self.dict["key1"], "value2")
-        self.assertEqual(self.array[0], 1)
-        self.group_stack.end(group_type=state.GROUP_TYPE.SIMPLE, position=1)
-        self.assertEqual(self.dict["key1"], "value1")
-        self.assertEqual(self.array[0], 0)
 
-    def test_set_global(self):
-        self.dict["key1"] = "value1"
-        self.array[0] = 0
-        self.group_stack.begin(group_type=state.GROUP_TYPE.SIMPLE, position=0)
-        self.dict["key1"] = "value2"
-        self.assertEqual(self.dict["key1"], "value2")
-        self.array[0] = 1
-        self.assertEqual(self.array[0], 1)
-        self.group_stack.begin(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
-        self.dict.setGlobal("key1", "value3")
-        self.assertEqual(self.dict["key1"], "value3")
-        self.array.setGlobal(0, 2)
-        self.assertEqual(self.array[0], 2)
-        self.group_stack.end(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
-        self.assertEqual(self.dict["key1"], "value3")
-        self.assertEqual(self.array[0], 2)
-        self.group_stack.end(group_type=state.GROUP_TYPE.SIMPLE, position=1)
-        self.assertEqual(self.dict["key1"], "value3")
-        self.assertEqual(self.array[0], 2)
+def test_set_global(groupstack):
+    stack, d, a = groupstack
+    d["key1"] = "value1"
+    a[1] = 1
+    stack.begin(group_type=state.GROUP_TYPE.SIMPLE, position=0)
+    d["key1"] = "value2"
+    assert d["key1"] == "value2"
+    a[1] = 0
+    assert a[1] == 0
+    stack.begin(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
+    d.setGlobal("key1", "value3")
+    assert d["key1"] == "value3"
+    a.setGlobal(1, -1)
+    assert a[1] == -1
+    stack.end(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=0)
+    assert d["key1"] == "value3"
+    assert a[1] == -1
+    stack.end(group_type=state.GROUP_TYPE.SIMPLE, position=1)
+    assert d["key1"] == "value3"
+    assert a[1] == -1
 
-    def test_group_mismatch(self):
-        try:
-            self.group_stack.begin(group_type=state.GROUP_TYPE.SIMPLE, position=0)
-            self.group_stack.end(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=1)
-        except ValueError as e:
-            pass
-        except Exception as e:
-            self.fail("unexpected exception: %s" % e)
-    
-    def test_parser(self):
-        parser = Parser()
-        parser.parse("\\count0=1\\begingroup\\count0=2")
-        self.assertEqual(parser.state.count[0], 2)
-        parser.parse("\\endgroup")
-        self.assertEqual(parser.state.count[0], 1)
-        try:
-            parser.parse("{\\endgroup")
-            self.fail("group matching failed")
-        except ValueError as e:
-            self.assertTrue("mismatch" in str(e))
-        try:
-            parser.parse("\\begingroup}")
-            self.fail("group matching failed")
-        except ValueError as e:
-            self.assertTrue("mismatch" in str(e))
-        
+def test_group_mismatch(groupstack):
+    stack, d, a = groupstack
+    try:
+        stack.begin(group_type=state.GROUP_TYPE.SIMPLE, position=0)
+        stack.end(group_type=state.GROUP_TYPE.SEMI_SIMPLE, position=1)
+    except ValueError as e:
+        pass
+    except Exception as e:
+        assert False, "unexpected exception: %s" % e
 
-if __name__ == '__main__':
-    unittest.main()
+def test_parser_group():
+    parser = Parser()
+    parser.parse("\\count0=1\\begingroup\\count0=2")
+    assert parser.state.count[0] == 2
+    parser.parse("\\endgroup")
+    assert parser.state.count[0] == 1
+
+def test_parser_group_mismatch():
+    parser = Parser()
+    try:
+        parser.parse("{\\endgroup")
+        assert False, "group matching failed"
+    except ValueError as e:
+        assert "mismatch" in str(e)
+    try:
+        parser.parse("\\begingroup}")
+        assert False, "group matching failed"
+    except ValueError as e:
+        assert "mismatch" in str(e)

9ccdea9d9b6febad7cd002c030dc7f72c865fa16
Junling Ma <junlingm@uvic.ca>
2025-01-06 01:55:04 -0800

    Migrate scanner tests to pytest

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,176 +1,107 @@
-import unittest
+import pytest
 from pytex.token import CATCODE
 from pytex import lexer
 
 
-class TestScanner(unittest.TestCase):
-    def catcode(self):
-        catcode = []
-        for c in range(256):
-            catcode.append(CATCODE.OTHER)
-        for c in range(ord("A"), ord("Z") + 1):
-            catcode[c] = CATCODE.LETTER
-            catcode[c + 32] = CATCODE.LETTER
-        catcode[ord("\\")] = CATCODE.ESCAPE
-        catcode[ord("{")] = CATCODE.BEGIN_GROUP
-        catcode[ord("}")] = CATCODE.END_GROUP
-        catcode[ord("\r")] = CATCODE.END_OF_LINE
-        catcode[ord(" ")] = CATCODE.SPACE
-        catcode[ord("\t")] = CATCODE.SPACE
-        catcode[ord("^")] = CATCODE.SUPERSCRIPT
-        catcode[ord("_")] = CATCODE.SUBSCRIPT
-        catcode[ord("$")] = CATCODE.MATH_SHIFT
-        catcode[ord("#")] = CATCODE.PARAMETER
-        catcode[ord("&")] = CATCODE.ALIGNMENT_TAB
-        catcode[ord("%")] = CATCODE.COMMENT
-        catcode[ord("@")] = CATCODE.ACTIVE
-        catcode[8] = CATCODE.INVALID
-        return catcode
-
-    def test_token(self):
-        catcode = self.catcode()
-        scanner = lexer.StringScanner(catcode, "A1{}^_$#&@")
-        token = scanner.read()
-        self.assertEqual(token.name, "A")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = scanner.read()
-        self.assertEqual(token.name, "1")
-        self.assertEqual(token.catcode, CATCODE.OTHER)
-        token = scanner.read()
-        self.assertEqual(token.name, "{")
-        self.assertEqual(token.catcode, CATCODE.BEGIN_GROUP)
-        token = scanner.read()
-        self.assertEqual(token.name, "}")
-        self.assertEqual(token.catcode, CATCODE.END_GROUP)
-        token = scanner.read()
-        self.assertEqual(token.name, "^")
-        self.assertEqual(token.catcode, CATCODE.SUPERSCRIPT)
-        token = scanner.read()
-        self.assertEqual(token.name, "_")
-        self.assertEqual(token.catcode, CATCODE.SUBSCRIPT)
-        token = scanner.read()
-        self.assertEqual(token.name, "$")
-        self.assertEqual(token.catcode, CATCODE.MATH_SHIFT)
-        token = scanner.read()
-        self.assertEqual(token.name, "#")
-        self.assertEqual(token.catcode, CATCODE.PARAMETER)
-        token = scanner.read()
-        self.assertEqual(token.name, "&")
-        self.assertEqual(token.catcode, CATCODE.ALIGNMENT_TAB)
-        token = scanner.read()
-        self.assertEqual(token.name, "@")
-        self.assertEqual(token.catcode, CATCODE.ACTIVE)
-        token = scanner.read()
-        self.assertEqual(token.catcode, CATCODE.SPACE)
-        token = scanner.read()
-        self.assertIsNone(token)
-
-    def test_command(self):
-        catcode = self.catcode()
-        scanner = lexer.StringScanner(catcode, "\\alpha 1\\beta\n")
-        token = scanner.read()
-        self.assertEqual(token.name, "\\alpha")
-        self.assertIsNone(token.catcode)
-        token = scanner.read()
-        self.assertEqual(token.name, "1")
-        self.assertEqual(token.catcode, CATCODE.OTHER)
-        token = scanner.read()
-        self.assertEqual(token.name, "\\beta")
-        self.assertIsNone(token.catcode)
-        token = scanner.read()
-        self.assertIsNone(token)
-
-    def test_comment(self):
-        catcode = self.catcode()
-        scanner = lexer.StringScanner(catcode, "A%comment")
-        token = scanner.read()
-        self.assertEqual(token.name, "A")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = scanner.read()
-        self.assertIsNone(token)
-
-    def test_space(self):
-        catcode = self.catcode()
-        scanner = lexer.StringScanner(catcode, "A  \tB")
-        token = scanner.read()
-        self.assertEqual(token.name, "A")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = scanner.read()
-        self.assertEqual(token.name, " ")
-        self.assertEqual(token.catcode, CATCODE.SPACE)
-        token = scanner.read()
-        self.assertEqual(token.name, "B")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = scanner.read()
-        self.assertEqual(token.catcode, CATCODE.SPACE)
-        token = scanner.read()
-        self.assertIsNone(token)
-
-    def test_eol(self):
-        catcode = self.catcode()
-        scanner = lexer.StringScanner(catcode, "A \n \n B")
-        token = scanner.read()
-        self.assertEqual(token.name, "A")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = scanner.read()
-        self.assertEqual(token.name, " ")
-        self.assertEqual(token.catcode, CATCODE.SPACE)
-        token = scanner.read()
-        self.assertEqual(token.name, "\\par")
-        self.assertIsNone(token.catcode)
-        token = scanner.read()
-        self.assertEqual(token.name, "B")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        p = scanner.position()
-        self.assertEqual(p.line, 3)
-        self.assertEqual(p.column, 2)
-        token = scanner.read()
-        self.assertEqual(token.catcode, CATCODE.SPACE)
-        token = scanner.read()
-        self.assertIsNone(token)
-
-    def test_expand(self):
-        catcode = self.catcode()
-        scanner = lexer.StringScanner(catcode, "^^61^^a")
-        token = scanner.read()
-        self.assertEqual(token.name, "a")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = scanner.read()
-        self.assertEqual(token.name, "!")
-        self.assertEqual(token.catcode, CATCODE.OTHER)
-        token = scanner.read()
-        self.assertEqual(token.catcode, CATCODE.SPACE)
-        token = scanner.read()
-        self.assertIsNone(token)
-
-    def test_input_stack(self):
-        catcode = self.catcode()
-        stack = lexer.InputStack()
-        scanner = lexer.StringScanner(catcode, "ABC")
-        stack.push(scanner)
-        token = stack.read()
-        self.assertEqual(token.name, "A")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        B = stack.read()
-        C = stack.read()
-        stack.unread(C)
-        stack.unread(B)
-        scanner = lexer.StringScanner(catcode, "1")
-        stack.push(scanner)
-        token = stack.read()
-        self.assertEqual(token.name, "1")
-        self.assertEqual(token.catcode, CATCODE.OTHER)
-        token = scanner.read()
-        self.assertEqual(token.catcode, CATCODE.SPACE)
-        token = stack.read()
-        self.assertEqual(token.name, "C")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = stack.read()
-        self.assertEqual(token.name, "B")
-        self.assertEqual(token.catcode, CATCODE.LETTER)
-        token = scanner.read()
-        self.assertIsNone(token)
+@pytest.fixture
+def catcode():
+    catcode = []
+    for c in range(256):
+        catcode.append(CATCODE.OTHER)
+    for c in range(ord("A"), ord("Z") + 1):
+        catcode[c] = CATCODE.LETTER
+        catcode[c + 32] = CATCODE.LETTER
+    catcode[ord("\\")] = CATCODE.ESCAPE
+    catcode[ord("{")] = CATCODE.BEGIN_GROUP
+    catcode[ord("}")] = CATCODE.END_GROUP
+    catcode[ord("\r")] = CATCODE.END_OF_LINE
+    catcode[ord(" ")] = CATCODE.SPACE
+    catcode[ord("\t")] = CATCODE.SPACE
+    catcode[ord("^")] = CATCODE.SUPERSCRIPT
+    catcode[ord("_")] = CATCODE.SUBSCRIPT
+    catcode[ord("$")] = CATCODE.MATH_SHIFT
+    catcode[ord("#")] = CATCODE.PARAMETER
+    catcode[ord("&")] = CATCODE.ALIGNMENT_TAB
+    catcode[ord("%")] = CATCODE.COMMENT
+    catcode[ord("@")] = CATCODE.ACTIVE
+    catcode[8] = CATCODE.INVALID
+    return catcode
 
 
-if __name__ == '__main__':
-    unittest.main()
+class Input:
+    def __init__(self, name, input, toks):
+        self.name = name
+        self.input = input
+        self.toks = toks
+
+
+class StringInput(Input):
+    def __init__(self, name, input):
+        toks = [t for t in input]
+        toks.append(" ")
+        super().__init__(name, input, toks)
+
+
+testdata = [
+    StringInput("tokens", "A1a{}^_$#&@"),
+    Input("commands", "\\alpha 1 \\beta", ["\\alpha", "1", " ", "\\beta"]),
+    Input("comment", "A%comment\n  1", ["A", "1", " "]),
+    Input("space", "A  \tB", ["A", " ", "B", " "]),
+    Input("eol", "A \n \n B", ["A", " ", "\\par", "B", " "]),
+    Input("expand",  "^^61^^a", ["a", "!", " "])
+]
+
+@pytest.mark.parametrize(
+    "input",
+    testdata,
+    ids = [test.name for test in testdata]
+)
+def test_token(catcode, input):
+    cat = []
+    for t in input.toks:
+        c = None if t[0] == "\\" else catcode[ord(t[0])]
+        cat.append(c)
+    scanner = lexer.StringScanner(catcode, input.input)
+    for i in range(len(input.toks)):
+        t = input.toks[i]
+        c = cat[i]
+        token = scanner.read()
+        assert token is not None
+        assert token.name == t
+        assert token.catcode == c
+    token = scanner.read()
+    assert token is None
+
+
+def test_input_stack(catcode):
+    stack = lexer.InputStack()
+    scanner = lexer.StringScanner(catcode, "ABC")
+    stack.push(scanner)
+    token = stack.read()
+    assert token is not None
+    assert token.name == "A"
+    assert token.catcode == CATCODE.LETTER
+    B = stack.read()
+    C = stack.read()
+    stack.unread(C)
+    stack.unread(B)
+    scanner = lexer.StringScanner(catcode, "1")
+    stack.push(scanner)
+    token = stack.read()
+    assert token is not None
+    assert token.name == "1"
+    assert token.catcode == CATCODE.OTHER
+    token = scanner.read()
+    assert token is not None
+    assert token.name == " "
+    assert token.catcode == CATCODE.SPACE
+    token = stack.read()
+    assert token is not None
+    assert token.name == "C"
+    assert token.catcode == CATCODE.LETTER
+    token = stack.read()
+    assert token is not None
+    assert token.name == "B"
+    assert token.catcode == CATCODE.LETTER
+    token = scanner.read()
+    assert token is None

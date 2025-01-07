b8feb1cc21abdf9440abdfa33c92c7bf74bc7a4a
Junling Ma <junlingm@uvic.ca>
2025-01-07 15:58:56 -0800

    Port test_toks.py to pytest

--- tests/test_toks.py
+++ tests/test_toks.py
@@ -1,47 +1,40 @@
-import unittest
-from pytex.parser import Parser
+import pytest
 
 
-class TestToks(unittest.TestCase):
-    def test_read_toks(self):
-        parser = Parser()
-        parser.readFrom("{abcd}")
-        k = parser.readBalancedText(expand=False)
-        self.assertEqual(len(k), 4)
-        self.assertEqual(k[3].name, "d")
+def test_read_toks(parser):
+    parser.readFrom("{abcd}")
+    k = parser.readBalancedText(expand=False)
+    assert len(k) == 4
+    assert k[3].name == "d"
     
-    def test_read_general_text(self):
-        parser = Parser()
-        parser.readFrom(" \\relax  {abcd}")
-        k = parser.readGeneralText(expand=False)
-        self.assertEqual(len(k), 4)
-        self.assertEqual(k[3].name, "d")
+
+def test_read_general_text(parser):
+    parser.readFrom(" \\relax  {abcd}")
+    k = parser.readGeneralText(expand=False)
+    assert len(k) == 4
+    assert k[3].name == "d"
 
 
-    def test_toks_register(self):
-        parser = Parser()
-        parser.parse("\\toks0={abcd}")
-        k = parser.state.toks[0]
-        self.assertEqual(len(k), 4)
-        self.assertEqual(k[3].name, "d")
+def test_toks_register(parser):
+    parser.parse("\\toks0={abcd}")
+    k = parser.state.toks[0]
+    assert len(k) == 4
+    assert k[3].name == "d"
 
-    def test_aftergroup(self):
-        parser = Parser()
-        parser.parse("\\aftergroup a\\aftergroup b{\\count0=1}")
-        self.assertEqual(str(parser.tokens), "ab ")
+
+def test_aftergroup(collector):
+    collector.parse("\\aftergroup a\\aftergroup b{\\count0=1}")
+    assert collector.getString() == "ab "
+
         
-    def test_case(self):
-        parser = Parser()
-        parser.parse("\\uppercase{a!}")
-        self.assertEqual(str(parser.tokens), "A! ")
-        parser.parse("\\lowercase{!A}")
-        self.assertEqual(str(parser.tokens), "!a ")
-        try:
-            parser.parse("\\catcode`z=13\\let z=a\\uppercase{azb}")
-            self.fail()
-        except ValueError as e:
-            self.assertIn("Z", str(e))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_case(collector):
+    collector.parse("\\uppercase{a!}")
+    assert collector.getString() == "A! "
+    collector.parse("\\lowercase{!A}")
+    assert collector.getString() == "!a "
+    collector.parse("\\catcode`z=13\\catcode`Z=13\\let Z=a\\uppercase{azb}")
+    assert collector.getString() == "AaB "
+    collector.parse("\\uppercase{a\\lowercase{Bc}}")
+    assert collector.getString() == "Abc "
+    collector.parse("\\uppercase{a\\def\\a{Bc}}\\a")
+    assert collector.getString() == "ABC"

bb61de6e6b5879c67f948043d5d54e5eac75bf31
Junling Ma <junlingm@uvic.ca>
2025-01-06 22:10:57 -0800

    Port test_arithmatic.py to pytest

--- tests/test_arithmatic.py
+++ tests/test_arithmatic.py
@@ -1,35 +1,24 @@
-import unittest
-from pytex.parser import Parser
+import pytest
+from tests import checkValues
 
-class TestReadDimen(unittest.TestCase):
-    def test_advance(self):
-        parser = Parser()
-        parser.parse("\\dimen0 = 10 pt\\advance \\dimen0 by 10 pt")
-        self.assertEqual(parser.state.dimen[0], 20)
-        parser.parse("{\\advance \\dimen0 by 10 pt")
-        self.assertEqual(parser.state.dimen[0], 30)
-        parser.parse("}")
-        self.assertEqual(parser.state.dimen[0], 20)
-        parser.parse("{\\global \\advance \\dimen0 by 10 pt}")
-        self.assertEqual(parser.state.dimen[0], 30)
-        try:
-            parser.parse("\\advance \\dimen0 by 10")
-            self.fail()
-        except Exception as e:
-            self.assertTrue('dimension unit expected' in str(e))
+
+def test_advance(parser):
+    checkValues(parser, "\\dimen0 = 10 pt\\advance \\dimen0 by 10 pt", [["dimen", 0, 20]])
+    checkValues(parser, "{\\advance \\dimen0 by 10 pt", [["dimen", 0, 30]])
+    checkValues(parser, "}", [["dimen", 0, 20]])
+    checkValues(parser, "{\\global \\advance \\dimen0 by 10 pt}", [["dimen", 0, 30]])
+
+def test_invalid_unit(parser):
+    try:
+        parser.parse("\\advance \\dimen0 by 10")
+        assert False, " a dimen must have a unit"
+    except Exception as e:
+        assert "dimension unit expected" in str(e)
         
-    def test_read_multiply(self):
-        parser = Parser()
-        parser.parse("\\dimen0 = 10 pt\\multiply \\dimen0 by 2 pt")
-        self.assertEqual(parser.state.dimen[0], 20)
-        self.assertEqual(str(parser.tokens), "pt ")
+def test_read_multiply(collector):
+    checkValues(collector, "\\dimen0 = 10 pt\\multiply \\dimen0 by 2 pt", [["dimen", 0, 20]])
+    assert collector.getString() == "pt "
 
     
-    def test_read_divide(self):
-        parser = Parser()
-        parser.parse("\\dimen0 = 10 pt\\divide \\dimen0 by 2")
-        self.assertEqual(parser.state.dimen[0], 5)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_read_divide(parser):
+    checkValues(parser, "\\dimen0 = 10 pt\\divide \\dimen0 by 2 pt", [["dimen", 0, 5]])

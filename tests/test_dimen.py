15827a8cd64fd81bcc13ecd903a6f4dd0773482b
Junling Ma <junlingm@uvic.ca>
2025-01-06 21:29:40 -0800

    Port test_dimen.py to pytest

--- tests/test_dimen.py
+++ tests/test_dimen.py
@@ -1,62 +1,50 @@
-import unittest
-from pytex.parser import Parser
+import pytest
+from tests import checkValues
 
-class TestReadDimen(unittest.TestCase):
-    def test_read_dimen(self):
-        parser = Parser()
-        parser.readFrom("10 pt")
-        result = parser.readDimen()
-        self.assertEqual(result, 10)
-        parser.readFrom("-10in")
-        result = parser.readDimen()
-        self.assertEqual(result, -10*72.27)
-        parser.readFrom("-1truept")
-        result = parser.readDimen()
-        self.assertEqual(result, -1)
-        parser.readFrom("-1 true pt")
-        result = parser.readDimen()
-        self.assertEqual(result, -1)      
-        
-    def test_read_mu(self):
-        parser = Parser()
-        parser.readFrom("10 mu")
-        result = parser.readDimen(mu=True)
-        self.assertEqual(result, 10)
-        try:
-            parser.readFrom("10 pt")
-            result = parser.readDimen(mu=True)
-            self.fail()
-        except Exception as e:
-            self.assertTrue('mu dimension expected' in str(e))
+
+def test_read_dimen(parser):
+    parser.readFrom("10 pt")
+    result = parser.readDimen()
+    assert result == 10
+    parser.readFrom("-10in")
+    result = parser.readDimen()
+    assert result == -10*72.27
+    parser.readFrom("-1Truept")
+    result = parser.readDimen()
+    assert result == -1
+    parser.readFrom("-1 true pt")
+    result = parser.readDimen()
+    assert result == -1
     
-    def test_read_dimen_with_invalid_token(self):
-        parser = Parser()
-        parser.readFrom("10 p")
-        try:
-            result = parser.readDimen()
-            self.fail()
-        except Exception as e:
-            print(e)
-            self.assertTrue('dimension unit expected' in str(e))
 
-    def test_dimen_array(self):
-        parser = Parser()
-        parser.parse("\\dimen0 = 10 pt")
-        self.assertEqual(parser.state.dimen[0], 10)
-        parser.parse("{\\dimen0 = 1 pt")
-        self.assertEqual(parser.state.dimen[0], 1)
-        parser.parse("}")
-        self.assertEqual(parser.state.dimen[0], 10)
-
-    def test_dimen_parameter(self):
-        parser = Parser()
-        parser.parse("\\hsize = 10 pt")
-        self.assertEqual(parser.state.layout["hsize"], 10)
-        parser.parse("{\\hsize = 1 pt")
-        self.assertEqual(parser.state.layout["hsize"], 1)
-        parser.parse("}")
-        self.assertEqual(parser.state.layout["hsize"], 10)
+def test_read_mu(parser):
+    parser.readFrom("10 mu")
+    result = parser.readDimen(mu=True)
+    assert result == 10
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_read_dimen_with_invalid_unit(parser):
+    parser.readFrom("10 pt")
+    try:
+        parser.readDimen(mu=True)
+        assert False, "cannot accept pt as unit when reading in mu dimension"
+    except Exception as e:
+        assert "mu dimension expected" in str(e)
+    parser.readFrom("10 p")
+    try:
+        parser.readDimen()
+        assert False, "cannot accept invalid unit"
+    except Exception as e:
+        assert "dimension unit expected" in str(e)
+
+
+def test_dimen_array(parser):
+    checkValues(parser, "\\dimen0 1 pt", [["dimen", 0, 1]])
+    checkValues(parser, "\\dimen0 = 10 pt", [["dimen", 0, 10]])
+    checkValues(parser, "{\\dimen0 = 1 pt", [["dimen", 0, 1]])
+    checkValues(parser, "}", [["dimen", 0, 10]])
+
+def test_dimen_parameter(parser):
+    checkValues(parser, "\\hsize = 10 pt", [["layout", "hsize", 10]])
+    checkValues(parser, "{\\hsize = 1 pt", [["layout", "hsize", 1]])
+    checkValues(parser, "}", [["layout", "hsize", 10]])

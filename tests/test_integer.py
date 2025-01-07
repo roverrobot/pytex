7d7c1ccecb11831af85d23d075ad5c9f1db4bff2
Junling Ma <junlingm@uvic.ca>
2025-01-06 21:20:27 -0800

    Port test_integer.py to pytest

--- tests/test_integer.py
+++ tests/test_integer.py
@@ -1,68 +1,64 @@
-import unittest
-from pytex.parser import Parser
+import pytest
 from pytex.token import Token, CATCODE
+from tests import checkValues
 
-class TestInteger(unittest.TestCase):
-    def test_read_integer(self):
-        parser = Parser()
-        parser.readFrom("123 a")
-        result = parser.readInteger()
-        self.assertEqual(result, 123)
-        t = parser.token_expand()
-        self.assertIsNotNone(t)
-        self.assertEqual(t.catcode, CATCODE.LETTER)
-        self.assertEqual(t.name, "a")
-        parser.readFrom("'10")
-        result = parser.readInteger()
-        self.assertEqual(result, 8)
-        parser.readFrom('"10')
-        result = parser.readInteger()
-        self.assertEqual(result, 16)
-        parser.readFrom("`a")
-        result = parser.readInteger()
-        self.assertEqual(result, 97)
-        parser.readFrom("`\\a")
-        result = parser.readInteger()
-        self.assertEqual(result, 97)
+
+def test_read_integer_dec(parser):
+    parser.readFrom("123 a")
+    result = parser.readInteger()
+    assert result == 123
+    t = parser.token_expand()
+    assert t is not None
+    assert t.catcode == CATCODE.LETTER
+    assert t.name == "a"
+
+def test_read_integer_oct(parser):
+    parser.readFrom("'10")
+    result = parser.readInteger()
+    assert result == 8
+
+def test_read_integer_hex(parser):
+    parser.readFrom('"10')
+    result = parser.readInteger()
+    assert result == 16
+
+def test_read_integer_char(parser):
+    parser.readFrom("`a")
+    result = parser.readInteger()
+    assert result == 97
+    parser.readFrom("`\\a")
+    result = parser.readInteger()
+    assert result == 97
 
     
-    def test_read_signed_integer(self):
-        parser = Parser()
-        parser.readFrom("-123")
-        result = parser.readInteger()
-        self.assertEqual(result, -123)
-        parser.readFrom("+123")
-        result = parser.readInteger()
-        self.assertEqual(result, 123)
-        parser.readFrom("--123")
-        result = parser.readInteger()
-        self.assertEqual(result, 123)
-        parser.readFrom("-+123")
-        result = parser.readInteger()
-        self.assertEqual(result, -123)
-        parser.readFrom("++123")
-        result = parser.readInteger()
-        self.assertEqual(result, 123)
-        try:
-            parser.readFrom("abc")
-            result = parser.readInteger()
-        except ValueError as e:
-            pass
+def test_read_signed_integer(parser):
+    parser.readFrom("-123")
+    result = parser.readInteger()
+    assert result == -123
+    parser.readFrom("+123")
+    result = parser.readInteger()
+    assert result == 123
+    parser.readFrom("--123")
+    result = parser.readInteger()
+    assert result == 123
+    parser.readFrom("-+123")
+    result = parser.readInteger()
+    assert result == -123
+    parser.readFrom("++123")
+    result = parser.readInteger()
+    assert result == 123
 
-    def test_integer_array(self):
-        parser = Parser()
-        parser.parse("\\count0=1")
-        self.assertEqual(parser.state.count[0], 1)
-        parser.parse("\\count0 2")
-        self.assertEqual(parser.state.count[0], 2)
-        parser.parse("\\count1=-\\count0")
-        self.assertEqual(parser.state.count[1], -2)
-        parser.parse("{\\count1=1")
-        self.assertEqual(parser.state.count[1], 1)
-        parser.parse("}")
-        self.assertEqual(parser.state.count[1], -2)
-        parser.parse("{\\global\\count1=1}")
-        self.assertEqual(parser.state.count[1], 1)
+def test_read_integer_error(parser):
+    try:
+        parser.readFrom("abc")
+        result = parser.readInteger()
+    except ValueError as e:
+        assert "integer" in str(e)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_integer_array(parser):
+    checkValues(parser, "\\count0=1", [["count", 0, 1]])
+    checkValues(parser, "\\count0 2", [["count", 0, 2]])
+    checkValues(parser, "\\count1=-\\count0", [["count", 1, -2]])
+    checkValues(parser, "{\\count1=1", [["count", 1, 1]])
+    checkValues(parser, "}", [["count", 1, -2]])
+    checkValues(parser, "{\\global\\count1=1}", [["count", 1, 1]])

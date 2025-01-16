61364fc5ea984807a7c7292194d693f3e5438952
Junling Ma <junlingm@uvic.ca>
2025-01-15 18:21:27 -0800

    Testing for \vskip and friends.

---
+++ tests/test_vmode.py
@@ -0,0 +1,21 @@
+import pytest
+from pytex import node as nd
+from pytex import glue
+
+@pytest.mark.parametrize(
+    "input,g", [
+        ["\\vskip 10pt", glue.Glue(10)],
+        ["\\vfil", glue.Glue(0, glue.Stretchness(1,1))],
+        ["\\vfill", glue.Glue(0, glue.Stretchness(1,2))],
+        ["\\vss", glue.Glue(0, glue.Stretchness(1,1), glue.Stretchness(1,1))],
+        ["\\vnegfil", glue.Glue(0, glue.Stretchness(-1,1))],
+    ]
+)
+def test_glue(parser, input, g):
+    parser.parse(input)
+    assert len(parser.lists) == 1
+    vlist = parser.lists[-1]
+    assert len(vlist) == 1
+    node = vlist[-1]
+    assert node.node_type == nd.NODE_TYPE.GLUE
+    assert node.glue == g

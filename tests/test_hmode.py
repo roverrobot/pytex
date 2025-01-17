536bb413962065dc22fef44bd5b1cfb132263121
Junling Ma <junlingm@uvic.ca>
2025-01-16 22:21:38 -0800

    some testing for homorizontal mode.

---
+++ tests/test_hmode.py
@@ -0,0 +1,63 @@
+import pytest
+from pytex import node as nd
+from pytex import glue
+from pytex import lists
+from pytex import texlive
+from pytex import hmode
+
+
+@pytest.fixture
+def cmr10(parser):
+    parser.parse('\\font\\f=cmr10 \\f')
+    return parser
+
+def test_new_paragraph(cmr10):
+    s = "Hello, world!"
+    cmr10.parse(s)
+    assert len(cmr10.lists) == 2
+    hlist = cmr10.lists[-1]
+    assert hlist.type == lists.LISTTYE.HORIZONTAL
+    assert len(hlist) == len(s)+2
+    node = hlist[0]
+    assert isinstance(node, hmode.IndentBox)
+    node = hlist[1]
+    assert node.node_type == nd.NODE_TYPE.CHAR
+    assert node.char  == "H"
+    node = hlist[7]
+    assert node.node_type == nd.NODE_TYPE.GLUE
+
+
+def test_par(cmr10):
+    cmr10.parse("hello\n\n")
+    assert len(cmr10.lists) == 1
+    vlist = cmr10.lists[-1]
+    assert vlist.type == lists.LISTTYE.VERTICAL
+    hlist = vlist[0]
+    assert hlist.type == lists.LISTTYE.HORIZONTAL
+    assert len(hlist) == 8 # indent, h, e, l, l, o, penalty(10000), glue,
+    node = hlist[0]
+    assert isinstance(node, hmode.IndentBox)
+    node = hlist[1]
+    assert node.node_type == nd.NODE_TYPE.CHAR
+    assert node.char == "h"
+    node = hlist[6]
+    assert node.node_type == nd.NODE_TYPE.PENALTY
+    assert node.penalty == 10000
+    node = hlist[7]
+    assert node.node_type == nd.NODE_TYPE.GLUE
+
+
+def test_vskip(cmr10):
+    cmr10.parse("hello\\vskip 1in\nworld\n\n")
+    assert len(cmr10.lists) == 1
+    vlist = cmr10.lists[-1]
+    assert vlist.type == lists.LISTTYE.VERTICAL
+    assert len(vlist) == 3
+    hlist = vlist[0]
+    assert hlist.type == lists.LISTTYE.HORIZONTAL
+    assert len(hlist) == 8
+    node = vlist[1]
+    assert node.node_type == nd.NODE_TYPE.GLUE
+    assert node.glue.dimen == 72.27
+    hlist = vlist[2]
+    assert hlist.type == lists.LISTTYE.HORIZONTAL

03c3e31bddcdc65691931bdee4b625fdf2acacb2
Junling Ma <junlingm@uvic.ca>
2025-01-24 22:25:58 -0800

    Properly check if a node is a \hbox

--- pytex/vmode.py
+++ pytex/vmode.py
@@ -30,13 +30,12 @@ class VList(lists.List):
             if isinstance(node, nd.Glue):
                 glues.append(node)
             elif node.node_type == nd.NODE_TYPE.HLIST:
-                nodes.append(node)
-                if not node.inner: 
+                if not node.hlist.inner:
                     # this is a paragraph. We have not implemented it yet
                     raise NotImplementedError("paragraphs are not implemented yet")
                 else:
                     # this is a \hbox.
-                    pass
+                    nodes.append(node)
                 for n in node.migrate:
                     if n.node_type == nd.NODE_TYPE.MARK:
                         nodes.append(n)

46d73cf2b1e30873e409d4a4b27f7fcc19dfe2ac
Junling Ma <junlingm@uvic.ca>
2026-03-01 00:14:47 -0800

    Track page builder state on main vlist

--- pytex/page.py
+++ pytex/page.py
@@ -12,6 +12,45 @@ from pytex.glue import Glue
 from pytex.dimen import Dimen
 
 
+class PageBuilderContext:
+    """
+    Snapshot of page-builder parameters that may change while building the main vlist.
+    """
+
+    def __init__(self, layout):
+        self.vsize = layout["vsize"]
+        self.topskip = layout["topskip"]
+        self.maxdepth = layout["maxdepth"]
+
+    def sameLayout(self, layout):
+        return (
+            self.vsize == layout["vsize"]
+            and self.topskip == layout["topskip"]
+            and self.maxdepth == layout["maxdepth"]
+        )
+
+
+class PageStateNode(nd.Node):
+    """
+    Transparent marker that records page-builder parameter changes on the main vlist.
+    """
+
+    node_type = nd.NODE_TYPE.WHATSIT
+
+    def __init__(self, context):
+        self.context = context
+
+    def saveInfo(self):
+        return {"init": {"context": self.context}}
+
+    @classmethod
+    def new(cls, parser, context):
+        return cls(context)
+
+    def __repr__(self):
+        return "PageState"
+
+
 class MainVList(vmode.VList):
     """
     The document's main vertical list.
@@ -19,6 +58,16 @@ class MainVList(vmode.VList):
 
     def __init__(self, parser):
         super().__init__(parser, inner=False)
+        self.page_initial_context = PageBuilderContext(parser.state.layout)
+        self.page_context = self.page_initial_context
+
+    def append(self, node):
+        if not isinstance(node, PageStateNode):
+            context = PageBuilderContext(self.parser.state.layout)
+            if not self.page_context.sameLayout(self.parser.state.layout):
+                super().append(PageStateNode(context))
+                self.page_context = context
+        super().append(node)
 
     @staticmethod
     def _pageMeasure(total, node):
@@ -87,11 +136,31 @@ class MainVList(vmode.VList):
 
     @staticmethod
     def _isNonDiscardable(node):
+        if isinstance(node, PageStateNode):
+            return False
         return node.node_type not in (
             nd.NODE_TYPE.GLUE,
             nd.NODE_TYPE.KERN,
             nd.NODE_TYPE.PENALTY,
         )
+
+    @staticmethod
+    def _isTransparent(node):
+        return isinstance(node, PageStateNode)
+
+    @classmethod
+    def _previousRealNode(cls, nodes, start, index):
+        index -= 1
+        while index >= start and cls._isTransparent(nodes[index]):
+            index -= 1
+        return index
+
+    @classmethod
+    def _nextRealNode(cls, nodes, index):
+        index += 1
+        while index < len(nodes) and cls._isTransparent(nodes[index]):
+            index += 1
+        return index
 
     @classmethod
     def _isLegalBreak(cls, nodes, start, index):
@@ -99,11 +168,13 @@ class MainVList(vmode.VList):
         if node.node_type == nd.NODE_TYPE.PENALTY:
             return True
         if node.node_type == nd.NODE_TYPE.GLUE:
-            if index <= start:
+            prev = cls._previousRealNode(nodes, start, index)
+            if prev < start:
                 return False
-            return cls._isNonDiscardable(nodes[index - 1])
+            return cls._isNonDiscardable(nodes[prev])
         if node.node_type == nd.NODE_TYPE.KERN:
-            return index + 1 < len(nodes) and nodes[index + 1].node_type == nd.NODE_TYPE.GLUE
+            nxt = cls._nextRealNode(nodes, index)
+            return nxt < len(nodes) and nodes[nxt].node_type == nd.NODE_TYPE.GLUE
         return False
 
     @staticmethod
@@ -120,96 +191,119 @@ class MainVList(vmode.VList):
         return Glue(total.dimen - excess, total.stretch, total.shrink)
 
     @staticmethod
-    def _prunePageTop(nodes, start):
-        while start < len(nodes) and nodes[start].node_type in (
-            nd.NODE_TYPE.GLUE,
-            nd.NODE_TYPE.KERN,
-            nd.NODE_TYPE.PENALTY,
-        ):
-            start += 1
-        return start
+    def _prunePageTop(nodes, start, context):
+        while start < len(nodes):
+            node = nodes[start]
+            if isinstance(node, PageStateNode):
+                context = node.context
+                start += 1
+                continue
+            if node.node_type in (nd.NODE_TYPE.GLUE, nd.NODE_TYPE.KERN, nd.NODE_TYPE.PENALTY):
+                start += 1
+                continue
+            break
+        return start, context
 
     @staticmethod
     def _candidateBreak(index, include):
         end = index + 1 if include else index
         return end, index + 1
 
-    def _bestPageBreak(self, nodes, start, goal, topskip, maxdepth):
+    def _bestPageBreak(self, nodes, start, context):
         total = Glue()
         topskip_added = False
         best = None
         bottom_depth = None
+        current_context = context
         for i in range(start, len(nodes)):
             node = nodes[i]
+            if isinstance(node, PageStateNode):
+                current_context = node.context
+                continue
             if not topskip_added:
-                top = self._pageTopskip(topskip, node)
+                top = self._pageTopskip(current_context.topskip, node)
                 if top is not None:
                     total = total + top
                     topskip_added = True
             if node.node_type == nd.NODE_TYPE.PENALTY:
                 if node.penalty >= 10000:
                     continue
-                effective = self._effectiveTotal(total, bottom_depth, maxdepth)
-                cost = self._pageCost(effective, goal, node.penalty)
-                current = (cost, i, False)
+                effective = self._effectiveTotal(total, bottom_depth, current_context.maxdepth)
+                cost = self._pageCost(effective, current_context.vsize, node.penalty)
+                current = (cost, i, False, current_context)
                 if best is None or cost <= best[0]:
                     best = current
                 if cost == inf or node.penalty <= -10000:
-                    _, index, include = best if best is not None else current
-                    return self._candidateBreak(index, include)
+                    _, index, include, best_context = best if best is not None else current
+                    end, next_start = self._candidateBreak(index, include)
+                    return end, next_start, best_context
                 continue
             self._pageMeasure(total, node)
             if self._hasDepth(node):
                 bottom_depth = node.depth
             if self._isLegalBreak(nodes, start, i):
-                effective = self._effectiveTotal(total, bottom_depth, maxdepth)
-                cost = self._pageCost(effective, goal, 0)
+                effective = self._effectiveTotal(total, bottom_depth, current_context.maxdepth)
+                cost = self._pageCost(effective, current_context.vsize, 0)
                 if best is None or cost <= best[0]:
-                    best = (cost, i, True)
-            if self._pageBadness(self._effectiveTotal(total, bottom_depth, maxdepth), goal) == inf and best is not None:
+                    best = (cost, i, True, current_context)
+            if self._pageBadness(
+                self._effectiveTotal(total, bottom_depth, current_context.maxdepth),
+                current_context.vsize,
+            ) == inf and best is not None:
                 break
         if best is None:
-            return len(nodes), len(nodes)
-        _, index, include = best
-        return self._candidateBreak(index, include)
+            return len(nodes), len(nodes), current_context
+        _, index, include, best_context = best
+        end, next_start = self._candidateBreak(index, include)
+        return end, next_start, best_context
 
-    def _buildPage(self, parser, nodes, start, end, maxdepth):
+    def _buildPage(self, parser, nodes, start, end, context):
         built = []
-        topskip = parser.state.layout["topskip"]
         topskip_added = False
         last_box = None
+        current_context = context
         for node in nodes[start:end]:
+            if isinstance(node, PageStateNode):
+                current_context = node.context
+                continue
             if not topskip_added:
-                top = self._pageTopskip(topskip, node)
+                top = self._pageTopskip(current_context.topskip, node)
                 if top is not None:
                     built.append(nd.Glue(top, "\\topskip"))
                     topskip_added = True
             built.append(node)
             if self._hasDepth(node):
                 last_box = node
-        if last_box is not None and last_box.depth > maxdepth:
-            last_box.depth = maxdepth
+        if last_box is not None and last_box.depth > current_context.maxdepth:
+            last_box.depth = current_context.maxdepth
         return built
+
+    @staticmethod
+    def _advanceContext(nodes, start, end, context):
+        for node in nodes[start:end]:
+            if isinstance(node, PageStateNode):
+                context = node.context
+        return context
 
     def pageBreak(self, parser):
         material = []
         self.typesetNodes(parser, material)
         pages = []
-        goal = parser.state.layout["vsize"]
-        topskip = parser.state.layout["topskip"]
-        maxdepth = parser.state.layout["maxdepth"]
+        context = self.page_initial_context
         start = 0
         while True:
-            start = self._prunePageTop(material, start)
+            start, context = self._prunePageTop(material, start, context)
             if start >= len(material):
                 break
-            end, next_start = self._bestPageBreak(material, start, goal, topskip, maxdepth)
+            end, next_start, break_context = self._bestPageBreak(material, start, context)
             if end <= start:
                 end = min(start + 1, len(material))
                 next_start = end
-            page = bx.VBox(parser, goal, Dimen())
-            page.list[:] = self._buildPage(parser, material, start, end, maxdepth)
+                break_context = self._advanceContext(material, start, end, context)
+            page = bx.VBox(parser, break_context.vsize, Dimen())
+            page.list[:] = self._buildPage(parser, material, start, end, context)
             page.typeset(parser, [])
             pages.append(page)
+            context = self._advanceContext(material, start, next_start, context)
             start = next_start
         return pages
--- tests/test_vmode.py
+++ tests/test_vmode.py
@@ -281,7 +281,7 @@ def test_page_break_glue_requires_non_discardable_predecessor(parser):
         nd.Glue(glue.Glue(4, glue.Stretchness(1)), None),
         second,
     ]
-    end, _ = main._bestPageBreak(nodes, 0, Dimen(10), glue.Glue(), Dimen())
+    end, _, _ = main._bestPageBreak(nodes, 0, page.PageBuilderContext(parser.state.layout))
     assert end == 1
 
 
@@ -294,7 +294,7 @@ def test_page_break_kern_requires_following_glue(parser):
         _test_hbox(parser, height=6, depth=0),
         nd.Glue(glue.Glue(), None),
     ]
-    end, _ = main._bestPageBreak(nodes, 0, Dimen(10), glue.Glue(), Dimen())
+    end, _, _ = main._bestPageBreak(nodes, 0, page.PageBuilderContext(parser.state.layout))
     assert end != 2
 
 
@@ -308,7 +308,7 @@ def test_page_break_prefers_later_equal_cost_breakpoint(parser):
         nd.Penalty(0),
         _test_hbox(parser, height=6, depth=0),
     ]
-    end, _ = main._bestPageBreak(nodes, 0, Dimen(10), glue.Glue(), Dimen())
+    end, _, _ = main._bestPageBreak(nodes, 0, page.PageBuilderContext(parser.state.layout))
     assert end == 3
 
 
@@ -319,7 +319,7 @@ def test_page_break_uses_maxdepth_in_cost_and_page_box(parser):
     total = glue.Glue(11, glue.Stretchness(2))
     effective = main._effectiveTotal(total, first.depth, parser.state.layout["maxdepth"])
     assert effective.dimen == 10
-    page_nodes = main._buildPage(parser, [first], 0, 1, parser.state.layout["maxdepth"])
+    page_nodes = main._buildPage(parser, [first], 0, 1, page.PageBuilderContext(parser.state.layout))
     assert page_nodes[0].node_type == nd.NODE_TYPE.GLUE
     assert page_nodes[1] is first
     assert first.depth == 2
@@ -334,3 +334,29 @@ def test_page_topskip_includes_rule(parser):
     assert pages[0].list[0].node_type == nd.NODE_TYPE.GLUE
     assert pages[0].list[0].name == "\\topskip"
     assert pages[0].list[1] is rule
+
+
+def test_main_vlist_inserts_page_state_marker_on_layout_change(parser):
+    main = parser.lists[0]
+    parser.state.layout["vsize"] = Dimen(20)
+    box = _test_hbox(parser, height=6, depth=0)
+    main.append(box)
+    assert len(main) == 2
+    assert isinstance(main[0], page.PageStateNode)
+    assert main[1] is box
+    assert main[0].context.vsize == 20
+
+
+def test_page_break_uses_marker_context(parser):
+    parser.parse("\\vsize=10pt\\topskip=0pt")
+    main = parser.lists[0]
+    first = _test_hbox(parser, height=6, depth=0)
+    second = _test_hbox(parser, height=6, depth=0)
+    main.append(first)
+    main.append(nd.Glue(glue.Glue(2, glue.Stretchness(1, 1)), None))
+    parser.state.layout["vsize"] = Dimen(20)
+    main.append(second)
+    main.append(nd.Penalty(-10000))
+    pages = main.pageBreak(parser)
+    assert len(pages) == 1
+    assert pages[0].height == 20
